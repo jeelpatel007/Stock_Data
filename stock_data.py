970114f9db3e6d932e00d03d4ee1acc784663e0b
Jeel Patel <jeelpatel007@yahoo.com>
2015-12-27 04:05:56 -0500

    Update stock_data.py

--- stock_data.py
+++ stock_data.py
@@ -1,9 +1,18 @@
 __author__ = 'jeel'
 
+# importing required modules for working with web-urls
 from urllib import request
 
+# This is a direct link to download the historical stock price of Google
 goog_url = "http://real-chart.finance.yahoo.com/table.csv?s=GOOG&d=11&e=27&f=2015&g=d&a=2&b=27&c=2014&ignore=.csv"
 
+'''
+Function 'download_stock_data' is the main function.
+It begins with recording the response (data) from the URL.
+Then the response is stored in a variable 'csv', which is later converted into string.
+Each line in the csv file is then split into new line.
+Later part creates a file 'goog.csv' and then populates it with the data we already collected.
+'''
 def download_stock_data(csv_url):
     response = request.urlopen(csv_url)
     csv = response.read()
